4106bb8f78f3c3c25089c89ef671bc836a92a18f
Abigail Addae <abigail2addae@gmail.com>
2022-07-01 20:45:21 +0000

    update query to prevent sql injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,13 +10,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryText = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+const values = [`%${process.argv[2]}%`, `${process.argv[3] || 5}`];
+
+pool.query(queryText, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,17 +6,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(
-  `
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2]}'
-  ORDER BY teacher;
-  `
-)
+const queryText =  `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`
+
+const values = [`${process.argv[2]}`];
+
+pool.query(queryText, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
